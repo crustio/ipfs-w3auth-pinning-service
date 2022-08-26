f5aede11c08cc5944004e0c9a56fea0c676cc9f3
LowEntropyBody <jszyyx@163.com>
2022-08-26 17:50:40 +0800

    fix err

--- src/middlewares/auth/authHandler.ts
+++ src/middlewares/auth/authHandler.ts
@@ -6,11 +6,13 @@ const Users = require('./../../models/Users');
 async function auth(req: Request, res: any, next: any) {
   const chainType = res.chainType;
   const address = res.chainAddress;
+  
   logger.info(`Validate chainType: ${chainType} address: ${address} success`);
+  logger.info(`txMsg: ${res.txMsg} tyMsg: ${res.tyMsg}`)
   // Find or create user
   const [user, created] = await Users.findOrCreate({
-    where: {chain_type: chainType, address: address},
-    defaults: {chain_type: chainType, address: address},
+    where: {chain_type: chainType, address: res.tyMsg},
+    defaults: {chain_type: chainType, address: res.tyMsg},
   });
   req.query.userId = user.id;
   next();
