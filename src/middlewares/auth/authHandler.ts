a06ee794c3c41b3d956493a371cdc0aa65139c06
LowEntropyBody <jszyyx@163.com>
2022-08-26 18:02:35 +0800

    fix err

--- src/middlewares/auth/authHandler.ts
+++ src/middlewares/auth/authHandler.ts
@@ -1,19 +1,28 @@
-import {Request} from 'express';
-import {logger} from '../../logger';
+import { Request } from 'express';
+import { logger } from '../../logger';
 const _ = require('lodash');
 const Users = require('./../../models/Users');
 
 async function auth(req: Request, res: any, next: any) {
   const chainType = res.chainType;
   const address = res.chainAddress;
-  
+
   logger.info(`Validate chainType: ${chainType} address: ${address} success`);
   logger.info(`txMsg: ${res.txMsg} tyMsg: ${res.tyMsg}`)
-  // Find or create user
-  const [user, created] = await Users.findOrCreate({
-    where: {chain_type: chainType, address: res.tyMsg},
-    defaults: {chain_type: chainType, address: res.tyMsg},
-  });
+  if (chainType == "xx") {
+    // Find or create user
+    const [user, created] = await Users.findOrCreate({
+      where: { chain_type: chainType, address: res.tyMsg },
+      defaults: { chain_type: chainType, address: res.tyMsg },
+    });
+  } else {
+    // Find or create user
+    const [user, created] = await Users.findOrCreate({
+      where: { chain_type: chainType, address: address },
+      defaults: { chain_type: chainType, address: address },
+    });
+  }
+
   req.query.userId = user.id;
   next();
 }
