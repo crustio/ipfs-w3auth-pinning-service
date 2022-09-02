86767abd8d3895b7ccfb47ba83ae51eb559110a7
LowEntropyBody <jszyyx@163.com>
2022-09-02 15:20:25 +0800

    Auth xx and aptos (#25)
    * use test auth handler
    
    * fix err
    
    * fix err
    
    * fix err
    
    * use 0.0.7

--- src/middlewares/auth/authHandler.ts
+++ src/middlewares/auth/authHandler.ts
@@ -1,17 +1,23 @@
-import {Request} from 'express';
-import {logger} from '../../logger';
+import { Request } from 'express';
+import { logger } from '../../logger';
 const _ = require('lodash');
 const Users = require('./../../models/Users');
 
 async function auth(req: Request, res: any, next: any) {
   const chainType = res.chainType;
-  const address = res.chainAddress;
+  let address = res.chainAddress;
+
   logger.info(`Validate chainType: ${chainType} address: ${address} success`);
+  logger.info(`txMsg: ${res.txMsg} tyMsg: ${res.tyMsg}`)
+  if (chainType == "xx") {
+    address = res.txMsg
+  } 
   // Find or create user
   const [user, created] = await Users.findOrCreate({
-    where: {chain_type: chainType, address: address},
-    defaults: {chain_type: chainType, address: address},
+    where: { chain_type: chainType, address: address },
+    defaults: { chain_type: chainType, address: address },
   });
+
   req.query.userId = user.id;
   next();
 }
